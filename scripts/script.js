b9cadca6a98b39a73fbab235b6fe963d3186fc73
Maxim Dzhenkov <maximimaximimaxim@yandex.ru>
2021-03-04 09:05:23 +0300

    fix/review/js

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,21 @@
 let showPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close-button');
-
-function showPopup() {
-    popup.classList.add('popup_opened');
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-showPopupButton.addEventListener('click', showPopup);
-closePopupButton.addEventListener('click', closePopup);
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__form-item_input_name');
 let jobInput = document.querySelector('.popup__form-item_input_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+function showPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.innerHTML;
+    jobInput.value = profileSubtitle.innerHTML;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -28,5 +24,7 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 
+showPopupButton.addEventListener('click', showPopup);
+closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
